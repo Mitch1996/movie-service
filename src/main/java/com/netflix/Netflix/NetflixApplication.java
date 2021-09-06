3c2d3563b1b71c5106510026a374ab459967a0dd
Mitch <mitchell.n@outlook.com>
2021-09-06 09:19:08 +0200

    Spring setup

---
+++ src/main/java/com/netflix/Netflix/NetflixApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.Netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixApplication.class, args);
+	}
+
+}
