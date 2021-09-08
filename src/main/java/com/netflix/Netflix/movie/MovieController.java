42aea59120a99635b0f9a285f8579820f6a6423b
Mitch <mitchell.n@outlook.com>
2021-09-08 11:00:02 +0200

    Made it so dicover uses values instead of hardcoded links

--- src/main/java/com/netflix/Netflix/movie/MovieController.java
+++ src/main/java/com/netflix/Netflix/movie/MovieController.java
@@ -13,53 +13,37 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/movies")
 public class MovieController {
 
-	@Autowired
-	RestTemplate restTemplate;
+    @Autowired
+    RestTemplate restTemplate;
 
-	@Value("${serviceURL}")
-	String serviceURL;
+    @Value("${serviceURL}")
+    String serviceURL;
 
-	@Value("${apiKey}")
-	String apiKey;
+    @Value("${apiKey}")
+    String apiKey;
 
-	@GetMapping("/all")
-	String getMovie() {
-		return restTemplate.getForObject(serviceURL + "/movie/550?api_key=" + apiKey, String.class);
-	}
+    @GetMapping("/full")
+    String getMovie() {
+        return restTemplate.getForObject(serviceURL + "movie/550?api_key=" + apiKey, String.class);
+    }
 
-	@RequestMapping("/{movieId}")
-	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-		MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
-		return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        MovieSummary movieSummary = restTemplate.getForObject(serviceURL + "movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 
-	}
-	@RequestMapping("/discover")
-	String getDiscoverMovie() {
-		String moviesToDiscover = restTemplate.getForObject("https://api.themoviedb.org/3/discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
-, String.class);
-		return moviesToDiscover;
+    }
 
-
-	}
-
-	@RequestMapping("/discover")
-	String getDiscoverMovieAscending() {
-		String moviesToDiscover = restTemplate.getForObject("https://api.themoviedb.org/3/discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
-				, String.class);
-		return moviesToDiscover;
+    @RequestMapping("/discover")
+    String getDiscoverMovie() {
+        String moviesToDiscover = restTemplate.getForObject(serviceURL + "discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate", String.class);
+        return moviesToDiscover;
 
 
-	}
+    }
 
-
-
-
-
-
-
-
-	@Bean
-	public RestTemplate rest() {
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate rest() {
+        return new RestTemplate();
+    }
 }
