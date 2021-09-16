6e22ec2e0865dc3e74e20ed7b83092c7266e872f
Mitch <mitchell.n@outlook.com>
2021-09-16 11:36:46 +0200

    Discover Endpoint with feign , MVC testing setup

--- src/main/java/com/netflix/Netflix/movie/MovieService.java
+++ src/main/java/com/netflix/Netflix/movie/MovieService.java
@@ -8,10 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "MovieDb", url = "${serviceURL}", path = "movie")
+@FeignClient(name = "MovieDb", url = "${serviceURL}")
 public interface MovieService {
 
     @GetMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> findById(@PathVariable Integer id, @RequestParam("api_key") String apiKey);
+
+    @GetMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&with_watch_monetization_types=flatrate")
+    ResponseEntity<String> dicovery(
+            @PathVariable("sort") String sort,
+            @PathVariable("include_vid") String include_vid,
+            @PathVariable("page") Integer page);
+
+
+    @GetMapping(value = "genre/movie/list?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US")
+    ResponseEntity<String> genre();
 }
+
+
+//api.themoviedb.org/3/genre/movie/list?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US
 
---
+++ src/test/java/com/netflix/Netflix/NetflixApplicationTestConfig.java
@@ -0,0 +1,42 @@
+package com.netflix.Netflix;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = NetflixApplication.class)
+@WebAppConfiguration
+public abstract class NetflixApplicationTestConfig {
+
+	protected MockMvc mvc;
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	protected void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	protected String mapToJson(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+	protected <T> T mapFromJson(String json, Class<T> clazz) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+}
