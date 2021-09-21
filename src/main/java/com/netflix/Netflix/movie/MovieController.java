69e1ed69d0fe1916269b5cff61e588ef2ad9dd49
Mitch <mitchell.n@outlook.com>
2021-09-21 10:40:57 +0200

    Added request mapping

--- src/main/java/com/netflix/Netflix/movie/MovieController.java
+++ src/main/java/com/netflix/Netflix/movie/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/api/v1")
 public class MovieController {
 
     @Autowired
