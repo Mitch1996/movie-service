28d454277b05c53889d095e0bd73ba517f5f16eb
Mitch <mitchell.n@outlook.com>
2021-09-21 10:15:22 +0200

    Can filter on genre now with discovery

--- src/main/java/com/netflix/Netflix/movie/MovieController.java
+++ src/main/java/com/netflix/Netflix/movie/MovieController.java
@@ -23,29 +23,80 @@ public class MovieController {
     @Value("${apiKey}")
     String apiKey;
 
-    @GetMapping("/full")
-    String getMovie() {
-        return restTemplate.getForObject(serviceURL + "movie/550?api_key=" + apiKey, String.class);
-    }
-
     @GetMapping("/{movieId}")
     public ResponseEntity<String> getMovieInfo(@PathVariable("movieId") Integer movieId) {
         return movieService.findById(movieId, apiKey);
     }
 
-<<<<<<< HEAD
     @GetMapping("discovery/{sort}/{include_vid}/{page}/{year}/{genre}")
-    public ResponseEntity<String> getDiscoverInfo( @PathVariable("sort") String sort, @PathVariable("include_vid") String include_vid, @PathVariable("page") Integer page , @PathVariable("year") Integer year, @PathVariable("genre") String genre) {
+    public ResponseEntity<String> getDiscoverInfo( @PathVariable("sort") String sort, @PathVariable("include_vid") String include_vid, @PathVariable("page") Integer page, @PathVariable("year") Integer year, @PathVariable("genre") String genre) {
+
+        switch (genre) {
+            case "Action":
+                genre = "28";
+                break;
+            case "Adventure":
+                genre = "12";
+                break;
+            case "Animation":
+                genre = "16";
+                break;
+            case "Comedy":
+                genre = "35";
+                break;
+            case "Crime":
+                genre = "80";
+                break;
+            case "Documentary":
+                genre = "99";
+                break;
+            case "Drama":
+                genre = "18";
+                break;
+            case "Family":
+                genre = "10751";
+                break;
+            case "Fantasy":
+                genre = "14";
+                break;
+            case "History":
+                genre = "36";
+                break;
+            case "Horror":
+                genre = "27";
+                break;
+            case "Music":
+                genre = "10402";
+                break;
+            case "Mystery":
+                genre = "9648";
+                break;
+            case "Romance":
+                genre = "10749";
+                break;
+            case "Science_Fiction":
+                genre = "878";
+                break;
+            case "TV_Movie":
+                genre = "10770";
+                break;
+            case "Thriller":
+                genre = "53";
+                break;
+            case "War":
+                genre = "10752";
+                break;
+            case "Western":
+                genre = "37";
+                break;
+        }
+
         return movieService.dicovery(sort, include_vid, page, year, genre);
     }
-=======
-    @RequestMapping("/discover")
-    String getDiscoverMovie() {
-        String moviesToDiscover = restTemplate.getForObject(serviceURL + "discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate", String.class);
-        return moviesToDiscover;
 
->>>>>>> parent of 6e22ec2 (Discover Endpoint with feign , MVC testing setup)
-
+    @GetMapping("genre")
+    public ResponseEntity<String> genre() {
+        return movieService.genre();
     }
 
     @Bean
--- src/main/java/com/netflix/Netflix/movie/MovieService.java
+++ src/main/java/com/netflix/Netflix/movie/MovieService.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "MovieDb", url = "${serviceURL}", path = "movie")
+@FeignClient(name = "MovieDb", url = "${serviceURL}")
 public interface MovieService {
 
     @GetMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> findById(@PathVariable Integer id, @RequestParam("api_key") String apiKey);
-<<<<<<< HEAD
 
-    @GetMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&primary_release_year={year}&&with_genres={genre}&with_watch_monetization_types=flatrate")
+    @GetMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&primary_release_year={year}&with_genres={genre}&with_watch_monetization_types=flatrate")
     ResponseEntity<String> dicovery(
             @PathVariable("sort") String sort,
             @PathVariable("include_vid") String include_vid,
@@ -30,7 +29,3 @@ public interface MovieService {
 
 
 
-=======
-}
-
->>>>>>> parent of 6e22ec2 (Discover Endpoint with feign , MVC testing setup)
