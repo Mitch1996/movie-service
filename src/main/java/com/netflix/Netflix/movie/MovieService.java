4f5ae8e30029093c7e6f100cea6d77baeb9fee87
Mitch <mitchell.n@outlook.com>
2021-09-21 13:08:26 +0200

    Attempt to fix heroku

--- src/main/java/com/netflix/Netflix/movie/MovieService.java
+++ src/main/java/com/netflix/Netflix/movie/MovieService.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "MovieDb", url = "${serviceURL}")
 public interface MovieService {
 
-    @RequestMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE , method = RequestMethod.GET)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
     ResponseEntity<String> findById(@PathVariable Integer id, @RequestParam("api_key") String apiKey);
 
     @RequestMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&primary_release_year={year}&with_genres={genre}&with_watch_monetization_types=flatrate" , method = RequestMethod.GET)
+    @ResponseBody
     ResponseEntity<String> dicovery(
             @PathVariable("sort") String sort,
             @PathVariable("include_vid") String include_vid,
