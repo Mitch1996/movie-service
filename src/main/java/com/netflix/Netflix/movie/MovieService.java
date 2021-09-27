532139a48bcb0d80b1d8b5455d1d1858b8d08f62
Mitch <mitchell.n@outlook.com>
2021-09-27 09:34:51 +0200

    test heroku commit

--- src/main/java/com/netflix/Netflix/movie/MovieService.java
+++ src/main/java/com/netflix/Netflix/movie/MovieService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "MovieDb", url = "${serviceURL}")
 public interface MovieService {
+    //test
     //  disney movies api =   "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_companies=6125&with_watch_monetization_types=flatrate"
     @RequestMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&with_companies={with_companies}&primary_release_year={year}&with_genres={genre}&with_watch_monetization_types=flatrate", method = RequestMethod.GET)
     @ResponseBody
