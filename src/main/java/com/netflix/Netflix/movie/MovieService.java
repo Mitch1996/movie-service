f9a6edcde70889cb5ec0244eaf49395d9ffa21b2
Mitch <mitchell.n@outlook.com>
2021-10-08 09:40:30 +0200

    Null fix

--- src/main/java/com/netflix/Netflix/movie/MovieService.java
+++ src/main/java/com/netflix/Netflix/movie/MovieService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "MovieDb", url = "${serviceURL}")
 public interface MovieService {
 
-    @RequestMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&with_companies={with_companies}&primary_release_year={year}&with_genres={genre}&with_watch_monetization_types=flatrate", method = RequestMethod.GET)
+    @RequestMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&primary_release_year={year}&with_genres={genre}&with_watch_monetization_types=flatrate", method = RequestMethod.GET)
     @ResponseBody
     ResponseEntity<String> discovery(
             @PathVariable("sort") String sort,
