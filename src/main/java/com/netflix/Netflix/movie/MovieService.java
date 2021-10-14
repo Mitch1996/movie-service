3d6687283726522fdf6a0f5290d838038c7ecc64
Izzet <izzetates85@gmail.com>
2021-10-14 16:44:22 +0300

    update Disney endpoint

--- src/main/java/com/netflix/Netflix/movie/MovieService.java
+++ src/main/java/com/netflix/Netflix/movie/MovieService.java
@@ -18,6 +18,7 @@ public interface MovieService {
             @PathVariable("genre") String genre);
 
 //&primary_release_year={year}
+//    update disne
     @RequestMapping(value = "discover/movie?api_key=97d7b8e2bab65af96c47f53519958733&language=en-US&sort_by=popularity.{sort}&include_adult=false&include_video={include_vid}&page={page}&with_companies={with_companies}&with_watch_monetization_types=flatrate", method = RequestMethod.GET)
     @ResponseBody
     ResponseEntity<String> Disney(
