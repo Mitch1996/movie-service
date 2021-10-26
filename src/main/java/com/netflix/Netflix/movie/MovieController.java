1b284e1061567e7c3c1e65e2625aaf327505cdcc
Mitch <mitchell.n@outlook.com>
2021-10-26 15:49:17 +0200

    crossorigin

--- src/main/java/com/netflix/Netflix/movie/MovieController.java
+++ src/main/java/com/netflix/Netflix/movie/MovieController.java
@@ -4,14 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 //import static com.netflix.Netflix.movie.MovieService.with_companies;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class MovieController {
